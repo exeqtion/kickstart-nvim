9139f19942214aace954ee1fb95324be6171f69c
exeqtion <exeqtion@protonmail.com>
2025-04-05 15:02:26 +0530

    c lsp

--- init.lua
+++ init.lua
@@ -644,7 +644,7 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible({"allPlugins", "start", "
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       -- NOTE: nixCats: there is help in nixCats for lsps at `:h nixCats.LSPs` and also `:h nixCats.luaUtils`
       local servers = {}
-        servers.clangd = {},
+        -- servers.clangd = {},
         -- servers.gopls = {},
         -- servers.pyright = {},
         -- servers.rust_analyzer = {},
